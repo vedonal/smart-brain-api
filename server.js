b3caaf2e28288551d70b2b4c93e5d9ef5f10f364
Kildon Veseli <kildonveseli1@gmail.com>
2020-06-25 23:41:36 +0200

    adding pg database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/Image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-silhouetted-69322',
     user: 'postgres',
     password: 'test123',
     database: 'smart-brain'
