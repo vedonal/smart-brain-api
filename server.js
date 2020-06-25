fdf0109905c62d6843c0c9cc9a1db4c2d4fdf422
Kildon Veseli <kildonveseli1@gmail.com>
2020-06-26 00:03:03 +0200

    added database connection

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-  ssl: true,
-  }
+    ssl: {
+    rejectUnauthorized: false,
+    },
+    },
 });
 
 const app = express();
